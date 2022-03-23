834b6f30991159b6b22845e153d522dfa58d0823
mark-labuschagne <marklabuschagne7@gmail.com>
2022-03-23 14:16:32 +0200

    Added a timeout for the service call in awaitHelper.sh To ensure that, if the container checking the await-helper has died, that the await-helper will not continue indefinitely. PLAT-175

--- awaitHelper.sh
+++ awaitHelper.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 responseOK="false"
+startTime=$(date +%s)
 while [ $responseOK != "true" ]; do
     response=$(curl --write-out "%{http_code}\n" --silent --output /dev/null "$@")
     if [ "$response" = "200" ]; then
         responseOK="true"
+    fi
+
+    if [ $(expr $currentTime - $startTime) -ge "60" ]; then
+        echo "Waited 60s with no response, exiting..."
+        exit 1
     fi
 
     sleep 1
