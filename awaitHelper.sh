855119953b954ef17474a392b7b759d90c4ec763
mark-labuschagne <marklabuschagne7@gmail.com>
2022-03-23 13:31:48 +0200

    Refactor the way input parameters are read To change inserting the URL into the awaitHelper.sh script through an env var to inserting it as a command line argument PLAT-175

--- awaitHelper.sh
+++ awaitHelper.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-serviceCallParam=$SERVICE_CALL_PARAM
-
 responseOK="false"
-while [ $responseOK != "true" ]
-do
-    response=$(curl --write-out "%{http_code}\n" --silent --output /dev/null $serviceCallParam)
+while [ $responseOK != "true" ]; do
+    response=$(curl --write-out "%{http_code}\n" --silent --output /dev/null "$@")
     if [ "$response" = "200" ]; then
         responseOK="true"
     fi
+
     sleep 1
 done
 
