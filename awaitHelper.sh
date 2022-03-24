9dab640b3b1c2daba6c914cb2e2643dcbc1938cf
mark-labuschagne <marklabuschagne7@gmail.com>
2022-03-24 11:52:01 +0200

    Refactored awaitHelper timeout logic
    PLAT-163

--- awaitHelper.sh
+++ awaitHelper.sh
@@ -3,17 +3,21 @@
 responseOK="false"
 startTime=$(date +%s)
 while [ $responseOK != "true" ]; do
+    sleep 1
+
+    currentTime=$(date +%s)
+    if [ $(expr $currentTime - $startTime) -ge 60 ] && [ $warned == "false" ]; then
+        echo "Warning: Waited 60s with no response. This is taking longer than it should..."
+        warned="true"
+    elif [ $(expr $currentTime - $startTime) -ge 130 ] && [ $warned == "true" ]; then
+        echo "Fatal: Waited 130s with no response. Exiting..."
+        exit 1
+    fi
+
     response=$(curl --write-out "%{http_code}\n" --silent --output /dev/null "$@")
     if [ "$response" = "200" ]; then
         responseOK="true"
     fi
-
-    if [ $(expr $currentTime - $startTime) -ge "60" ]; then
-        echo "Waited 60s with no response, exiting..."
-        exit 1
-    fi
-
-    sleep 1
 done
 
 echo "await-helper done"
