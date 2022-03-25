1b604c4b0900142385975efb723a62a2625fa7df
mark-labuschagne <marklabuschagne7@gmail.com>
2022-03-25 11:20:52 +0200

    Fixed bugs introduced by bad extention Shell-check extension suggested changes that introduced bugs PLAT-175

--- awaitHelper.sh
+++ awaitHelper.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
-responseOK="false"
-startTime=$(date +%s)
-while [ $responseOK != "true" ]; do
+ResponseOK="false"
+StartTime=$(date +%s)
+Warned="false"
+while [ $ResponseOK != "true" ]; do
     sleep 1
 
     currentTime=$(date +%s)
-    if [ $(expr $currentTime - $startTime) -ge 60 ] && [ $warned == "false" ]; then
+    if [ "$(($currentTime - $StartTime))" -ge 60 ] && [ $Warned = "false" ]; then
         echo "Warning: Waited 60s with no response. This is taking longer than it should..."
-        warned="true"
-    elif [ $(expr $currentTime - $startTime) -ge 130 ] && [ $warned == "true" ]; then
+        Warned="true"
+    elif [ "$(($currentTime - $StartTime))" -ge 130 ] && [ $Warned = "true" ]; then
         echo "Fatal: Waited 130s with no response. Exiting..."
         exit 1
     fi
 
     response=$(curl --write-out "%{http_code}\n" --silent --output /dev/null "$@")
     if [ "$response" = "200" ]; then
-        responseOK="true"
+        ResponseOK="true"
     fi
 done
 
