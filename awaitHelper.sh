e583d535ed873c5f628735ee2ad6c08a087b1e26
mark-labuschagne <marklabuschagne7@gmail.com>
2022-03-31 12:13:26 +0200

    Make requested clean-code changes To improve the logic, readability, and efficiency of the code in the awaitHelper.sh file. PLAT-175

--- awaitHelper.sh
+++ awaitHelper.sh
@@ -1,24 +1,20 @@
 #!/bin/sh
 
-ResponseOK="false"
-StartTime=$(date +%s)
-Warned="false"
-while [ $ResponseOK != "true" ]; do
-    sleep 1
+START_TIME=$(date +%s)
 
+Warned="false"
+echo "Waiting for $@"
+until [ $(curl --write-out "%{http_code}\n" --silent --output /dev/null "$@") = "200" ]; do
     currentTime=$(date +%s)
-    if [ "$(($currentTime - $StartTime))" -ge 60 ] && [ $Warned = "false" ]; then
+    if [ "$(($currentTime - $START_TIME))" -ge 60 ] && [ $Warned = "false" ]; then
         echo "Warning: Waited 60s with no response. This is taking longer than it should..."
         Warned="true"
-    elif [ "$(($currentTime - $StartTime))" -ge 130 ] && [ $Warned = "true" ]; then
+    elif [ "$(($currentTime - $START_TIME))" -ge 130 ] && [ $Warned = "true" ]; then
         echo "Fatal: Waited 130s with no response. Exiting..."
         exit 1
     fi
 
-    response=$(curl --write-out "%{http_code}\n" --silent --output /dev/null "$@")
-    if [ "$response" = "200" ]; then
-        ResponseOK="true"
-    fi
+    sleep 1
 done
 
 echo "await-helper done"
