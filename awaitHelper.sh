5120dbcb6f04660aa66d01893de9f8590c4c9e0c
mark-labuschagne <marklabuschagne7@gmail.com>
2022-03-31 12:23:23 +0200

    Set timeout check to 120s for fail case. For the sake of consistency. PLAT-175

--- awaitHelper.sh
+++ awaitHelper.sh
@@ -9,7 +9,7 @@ until [ $(curl --write-out "%{http_code}\n" --silent --output /dev/null "$@") = 
     if [ "$(($currentTime - $START_TIME))" -ge 60 ] && [ $Warned = "false" ]; then
         echo "Warning: Waited 60s with no response. This is taking longer than it should..."
         Warned="true"
-    elif [ "$(($currentTime - $START_TIME))" -ge 130 ] && [ $Warned = "true" ]; then
+    elif [ "$(($currentTime - $START_TIME))" -ge 120 ] && [ $Warned = "true" ]; then
         echo "Fatal: Waited 130s with no response. Exiting..."
         exit 1
     fi
