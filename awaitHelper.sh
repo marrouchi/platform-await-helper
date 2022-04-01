a2611210f09356ea3e39d096ca85d15c18a412e8
mark-labuschagne <marklabuschagne7@gmail.com>
2022-04-01 13:55:27 +0200

    Clean code
    PLAT-175

--- awaitHelper.sh
+++ awaitHelper.sh
@@ -2,15 +2,13 @@
 
 START_TIME=$(date +%s)
 
-Warned="false"
 echo "Waiting for $@"
 until [ $(curl --write-out "%{http_code}\n" --silent --output /dev/null "$@") = "200" ]; do
-    currentTime=$(date +%s)
-    if [ "$(($currentTime - $START_TIME))" -ge 60 ] && [ $Warned = "false" ]; then
+    timeDiff=$(($(date +%s) - $START_TIME))
+    if [ "$timeDiff" -ge 60 ] && [ "$timeDiff" -lt 61 ]; then
         echo "Warning: Waited 60s with no response. This is taking longer than it should..."
-        Warned="true"
-    elif [ "$(($currentTime - $START_TIME))" -ge 120 ] && [ $Warned = "true" ]; then
-        echo "Fatal: Waited 130s with no response. Exiting..."
+    elif [ "$timeDiff" -ge 120 ]; then
+        echo "Fatal: Waited 120s with no response. Exiting..."
         exit 1
     fi
 
